9cea21166eda3be2323439c90bdc70ef57c2aef4
Pham Anh Khoa <malaicarotgmail.com>
2024-04-11 11:08:56 +0700

    done

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:1115' : 'learn-it-full-stack-noul8ls3p-malaicarots-projects.vercel.app'
+export const API_URL = process.env.NODE_ENV !== 'production' ? 'learn-it-full-stack-noul8ls3p-malaicarots-projects.vercel.app' : ''
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learn_it_token'
