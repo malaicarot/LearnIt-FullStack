82a58a03209256fa84a029eb12fef02b1b920978
Pham Anh Khoa <malaicarotgmail.com>
2024-04-11 23:25:56 +0700

    add link server for production env

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:1115' : ''
+export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:1115' : 'https://learnit-fullstack-api.onrender.com'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learn_it_token'
