075c4a9bb68086da2d0ce7c852eaf7ba40e4e88e
Pham Anh Khoa <malaicarotgmail.com>
2024-04-11 23:11:57 +0700

    done

--- server/server.js
+++ server/server.js
@@ -28,7 +28,8 @@ app.use(ROUTER.POST.CONTEXT_PATH, PostRouter);
 const connectDB = async() => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS_WORD}@cluster0.to8ewaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      // `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS_WORD}@cluster0.to8ewaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      process.env.CONNECT_DB
     );
     console.log("waiting connect db!!");
     console.log("Connect successfully!!!");
@@ -38,7 +39,7 @@ const connectDB = async() => {
     });
   } catch (error) {
     console.log("Error while connect db!!!");
-    console.error({ message: error.msg });
+    console.error({ message: error.message });
   }
 }
 connectDB();
